b70d7b14b8f64a9741b97b8b71979d2559bc5e18
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2024-08-14 23:08:55 +0700

    setup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Cart />} path="/cart" />
+        <Route element={<PlaceOrder />} path="/order" />
+      </Routes>
     </div>
   );
 }
