11334fce0a6ff2d05055f1a2344bf2b6ca24519c
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2024-08-16 15:10:58 +0700

    code footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<Cart />} path="/cart" />
-        <Route element={<PlaceOrder />} path="/order" />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Cart />} path="/cart" />
+          <Route element={<PlaceOrder />} path="/order" />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
