e666f6630b78555952670d71aa1ecc7ae4a4450b
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2024-08-17 23:41:22 +0700

    code cart page and place order

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 // eslint-disable-next-line react/prop-types
 function Navbar({ setShowLogin }) {
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -43,8 +47,10 @@ function Navbar({ setShowLogin }) {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
